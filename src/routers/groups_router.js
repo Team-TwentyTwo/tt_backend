85388b57620ad893d3b8311f4ce710e4e8f08f7a
Choi Jong Yun <koreallama101@gmail.com>
2024-08-14 20:12:21 +0900

    Feat: 그룹 라우터 4개 구현

--- src/routers/groups_router.js
+++ src/routers/groups_router.js
@@ -7,19 +7,39 @@ app.post('/groups', asyncHandler(async (req, res) => {
 }))
 
 app.get('/groups', asyncHandler(async (req, res) => {
-  
+  const groups = await prisma.user.findMany();
+
+  res.send(groups);
 }))
 
-app.put('/groups/:groupId', asyncHandler(async (req, res) => {
-  
+app.patch('/groups/:groupId', asyncHandler(async (req, res) => {
+  assert(req.body, PatchGroup)
+
+  const { groupId } = req.params;
+  const group = await prisma.group.update({
+    where: { groupId },
+    data: req.body,
+  })
+
+  res.send(group);
 }))
 
 app.delete('/groups/:groupId', asyncHandler(async (req, res) => {
-  
+  const { groupId } = req.params;
+  await prisma.group.delete({
+    where: { groupId },
+  })
+
+  res.sendStatus(204);
 }))
 
 app.get('/groups/:groupId', asyncHandler(async (req, res) => {
-  
+  const { groupId } = req.params;
+  const group = await prisma.group.findUniqueOrThrow({
+    where: { groupId },
+  });
+
+  res.send(group);
 }))
 
 app.post('/groups/:groupId/verify-password', asyncHandler(async (req, res) => {
