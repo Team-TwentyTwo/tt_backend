1c4c0470f2e0e9d798de844df27b600f0b6e196d
yujeong <parkyj0430@gmail.com>
2024-08-17 01:50:49 +0900

    Feat : 게시글 삭제 API 구현

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -30,3 +30,31 @@ export const editPost = asyncHandler(async (req, res) => {
 
     res.status(200).json(newPost);
 });
+
+
+/* -------------------- 게시글 삭제 -------------------- */
+export const deletePost = asyncHandler(async (req, res) => {
+    const { postId } = req.params;
+    const { postPassword } = req.body;
+    
+    const post = await prisma.posts.findUniqueOrThrow({
+        where: { id: postId }
+    });
+
+    if (post.postPassword !== postPassword) {
+        throw { name: 'ForbiddenError' };
+    }
+
+    // 게시글 삭제
+    await prisma.posts.delete({
+        where: { id: postId }
+    });
+
+    // 그룹의 postCount 1 감소
+    await prisma.groups.update({
+        where: { id: post.groupId },
+        data: { postCount: { decrement: 1 } }
+    });
+
+    res.status(200).json({ message: '게시글 삭제 성공' });
+});
--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { editPost } from '../controllers/postController.js';
+import { editPost, deletePost } from '../controllers/postController.js';
 
 
 const router = express.Router();
 
 router.patch('/:postId', editPost) // 게시글 수정
+router.delete('/:postId', deletePost); // 게시글 삭제
 
 
 export default router;
