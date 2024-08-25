af4fcc23e9aef0b7f094adfbcbeabfb9795f3a81
yujeong <parkyj0430@gmail.com>
2024-08-25 10:29:50 +0900

    Feat : 댓글 삭제 API 구현

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -4,7 +4,7 @@ import { assert } from 'superstruct';
 import { PatchComment } from "../structs.js";
 
 /* -------------------- 댓글 수정 -------------------- */
-export const EditComment = asyncHandler(async (req, res) => {
+export const editComment = asyncHandler(async (req, res) => {
     const { commentId } = req.params;
 
     // 유효성 검사 수행
@@ -33,4 +33,30 @@ export const EditComment = asyncHandler(async (req, res) => {
     });
 
     res.status(200).json(newComment);
+});
+
+
+/* -------------------- 댓글 삭제 -------------------- */
+export const deleteComment = asyncHandler(async (req, res) => {
+    const { commentId } = req.params;
+    const { password } = req.body;
+
+    const comment = await prisma.comments.findUniqueOrThrow({
+        where: { id: commentId }
+    });
+
+    if (comment.password !== password) {
+        throw { name: 'ForbiddenError' };
+    }
+
+    await prisma.comments.delete({
+        where: { id: commentId }
+    });
+
+    await prisma.posts.update({
+        where: { id: comment.postId },
+        data: { commentCount: { decrement: 1 } }
+    });
+
+    res.status(200).json({ message: '답글 삭제 성공' });
 })
--- src/routers/commentRouter.js
+++ src/routers/commentRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { EditComment } from '../controllers/commentController.js';
+import { editComment, deleteComment } from '../controllers/commentController.js';
 
 const router = express.Router();
 
-router.patch('/:commentId', EditComment); // 댓글 수정
+router.patch('/:commentId', editComment); // 댓글 수정
+router.delete('/:commentId', deleteComment); // 댓글 삭제
 
 
 export default router;
